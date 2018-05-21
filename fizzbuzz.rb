5f9c8fee3cef4f344a7a7da3cdd29f3ff31b7375
Lillian Zhang <lilyzhangyun@gmail.com>
2018-05-21 21:41:25 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz (n)
   if n % 3 == 0
-    return
+    return "Fizz"
   elsif n % 5 == 0
-    return
+    return "Buzz"
   elsif n % (3*5) == 0
-    return
+    return "FizzBuzz"
   else 
     return nil 
   end 
